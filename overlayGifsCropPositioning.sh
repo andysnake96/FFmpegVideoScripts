ecc20a27aff0a06c103b009e8507f5b793e6e3aa
andysnake <andysnake@acer.acerDom>
2021-03-23 19:52:49 +0100

    overlay1x1added

--- overlayGifsCropPositioning.sh
+++ overlayGifsCropPositioning.sh
@@ -18,25 +18,31 @@
 #identify $1 $2 $3 $4
 echo "configurable overlay with env exportable vars: W H OVERLAY_SIZE (either 2x2(dlft) 3x3) and offset from botton OFFB (dflt 0)"
 echo "usage: vids inputs [outFname (dflt output.mp4)]"
-w=400
-h=225
+w=640
+h=480
 offb=0
 offl=0		#TODO NOT DONE. .. sensitive image loss :(
 if [[ $W ]];then w=$W;fi
 if [[ $H ]];then h=$H;fi
 if [[ $OFFB ]];then offb=$OFFB;fi
 if [[ $OFFL ]];then offl=$OFFL;fi
-ffmpegBuildPath="/home/andysnake/ffmpeg/bin/ffmpeg_nv"
-encoder="h264_nvenc"
+ffmpegBuildPath="/home/andysnake/ffmpeg"
+encoder="" #-c:v h264_nvenc "
 outName="output.mp4"
 #LOGLEVEL="-loglevel debug"
 
-if [[ ! $OVERLAY_SIZE ]];then OVERLAY_SIZE="2x2";fi #dflt overlay size
+if [[ ! $OVERLAY_SIZE ]];then OVERLAY_SIZE="1x1";fi #dflt overlay size
 #timeStamp="$(date --iso-8601=minutes | perl -pe 's/[^0-9]+//g')"
 
-if [[ $OVERLAY_SIZE == "2x2" ]];then
+if [[ $OVERLAY_SIZE == "1x1" ]];then
+
+	$ffmpegBuildPath -y $LOGLEVEL  -hide_banner -i $1 -i $2 -filter_complex " \
+		nullsrc=size=$(( $w * 2))x$h [base];[base][0:v] overlay=shortest=1 [tmp1];\
+		[tmp1][1:v] overlay=shortest=1:x=$w	"  $encoder $outName
+
+elif [[ $OVERLAY_SIZE == "2x2" ]];then
 	if [[ $5 ]];then outName=$5;fi
-	/home/andysnake/ffmpeg/bin/ffmpeg_nv -y $LOGLEVEL  -hide_banner -i $1 -i $2 -i $3 -i $4  -filter_complex " \
+	$ffmpegBuildPath -y $LOGLEVEL  -hide_banner -i $1 -i $2 -i $3 -i $4  -filter_complex " \
 		nullsrc=size=$(( $w * 2))x$(( ($h - $offb) * 2)) 		[base];\
 		[base][0:v] overlay=shortest=1 					[tmp1];\
 		[tmp1][1:v] overlay=shortest=1:x=$w				[tmp2];\
@@ -45,7 +51,7 @@ if [[ $OVERLAY_SIZE == "2x2" ]];then
 		-c:v $encoder $outName
 elif [[ $OVERLAY_SIZE == "3x3" ]];then
 	if [[ ${10} ]];then outName=${10};fi
-	/home/andysnake/ffmpeg/bin/ffmpeg_nv -y $LOGLEVEL -hide_banner -i $1 -i $2 -i $3 -i $4 -i $5 -i $6 -i $7  -i $8 -i $9 -filter_complex " \
+	$ffmpegBuildPath -y $LOGLEVEL -hide_banner -i $1 -i $2 -i $3 -i $4 -i $5 -i $6 -i $7  -i $8 -i $9 -filter_complex " \
 		nullsrc=size=$(( $w * 3))x$(( ($h - $offb ) * 3 ))			[base];\
 		[base][0:v] overlay=shortest=1:						[tmp1];\
 		[tmp1][1:v] overlay=shortest=1:x=$(($w - 40))				[tmp2];\
